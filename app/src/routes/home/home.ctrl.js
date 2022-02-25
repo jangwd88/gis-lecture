3b6a3deeba7660f16be25c524198ce34822fe529
Wonder Chang <jangwd88@gmail.com>
2022-02-25 17:39:59 +0900

    fetch 수정 해야함

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict"
 
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -9,9 +10,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["wonder", "chang", "jangwd88"],
+    passwd: ["1234", "12345", "123456", ],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id;
+        const passwd = req.body.passwd;
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.passwd[idx] === passwd){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패하였습니다.",
+        });
     },
 };
 
