8e5dc714db450824bf6ec034a73a7ca245138515
Wonder Chang <jangwd88@gmail.com>
2022-02-25 14:30:43 +0900

    프론트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`)); // 정적 경로 설정
 app.use("/", home); // use -> 미들웨어 등록 메서드
 
 module.exports = app;
