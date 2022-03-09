9ee2338958d63c4639ba892fb29ed4795a2f538f
Wonder Chang <jangwd88@gmail.com>
2022-03-09 11:11:09 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,25 +1,11 @@
 "use strict"
 
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users = {
-        id: ["wonder", "chang", "jangwd88"],
-        passwd: ["1234", "12345", "123456", ],
-        name: ["원더", "췡", "jangwd88", ],
-    };
 
-    static getUsers(...fileds){
-        const users = this.#users;
-        const newUsers = fileds.reduce((newUsers, filed) => {
-            if(users.hasOwnProperty(filed)){
-                newUsers[filed] = users[filed];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-
-    static getUserInfo(id){
-        const users = this.#users;
+    static #getUserInfo(data, id){
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users); 
         const userInfo = usersKeys.reduce((newUser, info) => {
@@ -30,9 +16,28 @@ class UserStorage {
         return userInfo;
     }
 
+    static getUsers(...fileds){
+        // const users = this.#users;
+        const newUsers = fileds.reduce((newUsers, filed) => {
+            if(users.hasOwnProperty(filed)){
+                newUsers[filed] = users[filed];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+    static getUserInfo(id){
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);  
+    }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.passwd.push(userInfo.passwd);
